34f111cad4e04f7c309cf8b777a4a77a62a60cc2
Christopher Small <metasoarous@gmail.com>
2015-03-18 12:31:54 -0700

    Moved dev deps into :dev profile; updated other dependencies

--- project.clj
+++ project.clj
@@ -6,13 +6,9 @@
   :eval-in-leiningen true
   :signing {:gpg-key "F5DE1E30"}
   :source-paths ["./src"]
-  :dependencies
-  [
-    [org.clojure/clojure "1.5.1"]
-    [org.clojure/math.numeric-tower "0.0.2"]
-    [org.clojure/data.zip "0.1.1"]
-    [org.clojure/tools.namespace "0.2.0"]
-    [clj-ns-browser "1.3.1"]
-    [lacij "0.8.0"]
-  ]
-)
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/tools.namespace "0.2.10"]
+                 [lacij "0.9.0" :exclusions [org.clojure/clojure]]]
+  :profiles {:dev {:dependencies [[org.clojure/math.numeric-tower "0.0.2"]
+                                  [clj-ns-browser "1.3.1"]]}})
