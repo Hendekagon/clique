41830246437c054c73bc52797fcd7beb21b7e4e1
Christopher Small <metasoarous@gmail.com>
2015-03-23 01:58:52 -0700

    Added apply-kwargs helper

--- src/clique/impl/core.clj
+++ src/clique/impl/core.clj
@@ -27,3 +27,14 @@
        (map zip/node)
        (filter symbol?)))
 
+(defn apply-kwargs
+  "Utility that takes a function f, any number of regular args, and a final kw-args argument which will be
+  splatted in as a final argument"
+  [f & args]
+  (apply
+    (apply partial
+           f
+           (butlast args))
+    (apply concat (last args))))
+
+
